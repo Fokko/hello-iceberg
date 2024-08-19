0e227f33372aeb9356fb5ac7744ac14bf2146a78
Fokko <fokko@apache.org>
2024-08-19 11:43:52 +0200

    Test 0.3.0 RC1

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
+use std::collections::HashMap;
 use iceberg_catalog_rest::{RestCatalog, RestCatalogConfig};
 use iceberg::Catalog;
 use iceberg::TableIdent;
+use std::env;
 
 #[tokio::main]
 async fn main() {
-    let config = RestCatalogConfig::builder().uri("https://api.dev.tabulardata.io/ws/".to_string()).build();
+    let config = RestCatalogConfig::builder()
+        .uri("https://api.tabular.io/ws".to_string())
+        .warehouse("Fokko".to_string())
+        .props(HashMap::from([
+            ("credential".to_string(), env::var("credential").unwrap())
+        ])).build();
 
-    let catalog = RestCatalog::new(config).await.unwrap();
+    let catalog = RestCatalog::new(config);
 
     let all_namespaces = catalog.list_namespaces(None).await.unwrap();
     println!("Namespaces in current catalog: {:?}", all_namespaces);
 
-    let table2 = catalog
-        .load_table(&TableIdent::from_strs(["nyc", "taxis"]).unwrap())
-        .await
-        .unwrap();
-    println!("{:?}", table2.metadata());
+    let wrapped = catalog
+        .load_table(&TableIdent::from_strs(["examples", "nyc_taxi_locations"]).unwrap())
+        .await;
 
-    println!("Hello, world!");
+    let table2 = wrapped.unwrap() ;
+
+    println!("{:?}", table2.metadata());
+    println!("{:?}", table2.scan().build().unwrap());
 }
